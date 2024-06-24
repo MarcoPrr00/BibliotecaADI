d8a13c343302f822418b5674c28fab657ceba627
MarcoPrr00 <m.porro14@studenti.uniba.it>
2024-06-24 16:52:51 +0200

    piccoli aggiustamenti

--- update_book.php
+++ update_book.php
@@ -7,6 +7,7 @@ $titolo = $_POST['titolo'];
 $numero_pagine = $_POST['numero_pagine'];
 $casa_editrice = $_POST['casa_editrice'];
 $quantita = $_POST['quantita'];
+$prezzo = $_POST['prezzo'];
 $dataInizio = isset($_POST['dataInizio']) ? $_POST['dataInizio'] : null;
 $dataFine = isset($_POST['dataFine']) ? $_POST['dataFine'] : null;
 $id_autore = $_POST['autore'];
@@ -18,18 +19,18 @@ try {
         $sql = "UPDATE Libro L
                 JOIN LibroAutore LA ON L.ID_libro = LA.ID_libro
                 JOIN Autore A ON LA.ID_autore = A.ID_autore
-                SET L.ISBN = ?, L.Titolo = ?, L.Numero_pagine = ?, L.Casa_editrice = ?, L.quantita = ?, LA.ID_autore = ?
+                SET L.ISBN = ?, L.Titolo = ?, L.Numero_pagine = ?, L.Casa_editrice = ?, L.quantita = ?, LA.ID_autore = ?, L.prezzo = ?
                 WHERE L.ID_libro = ?";
         $stmt = $conn->prepare($sql);
-        $stmt->bind_param("ssisiii", $isbn, $titolo, $numero_pagine, $casa_editrice, $quantita, $id_autore, $ID_libro);
+        $stmt->bind_param("ssisiisi", $isbn, $titolo, $numero_pagine, $casa_editrice, $quantita, $id_autore, $prezzo, $ID_libro);
     }else{
         $sql = "UPDATE Libro L
                 JOIN LibroAutore LA ON L.ID_libro = LA.ID_libro
                 JOIN Autore A ON LA.ID_autore = A.ID_autore
-                SET L.ISBN = ?, L.Titolo = ?, L.Numero_pagine = ?, L.Casa_editrice = ?, L.quantita = ?, LA.ID_autore = ?, L.Data_inizio_prestito = ?, L.Data_fine_prestito = ?
+                SET L.ISBN = ?, L.Titolo = ?, L.Numero_pagine = ?, L.Casa_editrice = ?, L.quantita = ?, LA.ID_autore = ?, L.prezzo = ?, L.Data_inizio_prestito = ?, L.Data_fine_prestito = ?
                 WHERE L.ID_libro = ?";
         $stmt = $conn->prepare($sql);
-        $stmt->bind_param("ssisiissi", $isbn, $titolo, $numero_pagine, $casa_editrice, $quantita, $id_autore,$dataInizio, $dataFine, $ID_libro);
+        $stmt->bind_param("ssisiisssi", $isbn, $titolo, $numero_pagine, $casa_editrice, $quantita, $id_autore, $prezzo, $dataInizio, $dataFine, $ID_libro);
     }
     $stmt->execute();
 
